bb68b32d63624c0682d4dffdf8278d20fd03eeda
htakimsean <dustlrka86@gmail.com>
2025-06-18 11:06:53 +0900

    config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import vue from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [react()],
+  base:"/my-app/"
 })
